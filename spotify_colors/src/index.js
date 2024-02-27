01ef9bad610491f65e16f3661f5a19218a23fdf3
Adam McFry <armcfry@gmail.com>
2024-02-27 15:23:54 -0800

    initial progress, attempting auth workflow

--- spotify_colors/src/index.js
+++ spotify_colors/src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
+import Connection from './components/Connection.tsx'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <h1/>Spotify Colors Project<h1/>
+    <Connection></Connection>
   </React.StrictMode>
 );
 
